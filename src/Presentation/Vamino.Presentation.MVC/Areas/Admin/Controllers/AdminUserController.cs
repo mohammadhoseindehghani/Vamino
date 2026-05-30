1f52c875be8ce6faef8fc889e3afa6b8d1b06d94
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2026-05-30 22:09:39 +0330

    import admin theme

---
+++ src/Presentation/Vamino.Presentation.MVC/Areas/Admin/Controllers/AdminUserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vamino.Presentation.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
