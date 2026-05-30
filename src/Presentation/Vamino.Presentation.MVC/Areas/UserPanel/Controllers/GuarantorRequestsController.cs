b9d9cdd6e8cf081b11f9b29e6ca08935cf54ad23
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2026-05-30 21:44:37 +0330

    import theme for userPanel

---
+++ src/Presentation/Vamino.Presentation.MVC/Areas/UserPanel/Controllers/GuarantorRequestsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vamino.Presentation.MVC.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class GuarantorRequestsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Presentation/Vamino.Presentation.MVC/Areas/UserPanel/Controllers/LoanContractsController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Vamino.Presentation.MVC.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    //[Authorize]
+    public class LoanContractsController(IMediator mediator) : Controller
+    {
+
+        public async Task<IActionResult> Index()
+        {
+            return View(); 
+        }
+
+        public IActionResult Create() => View();
+        public IActionResult Delete() => View();
+        public IActionResult Edit() => View();
+
+    }
+}
